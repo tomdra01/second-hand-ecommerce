1aced812c0f98d6740097881a48f34fa77e4b5d7
Tomáš Dračka <tomidracka@gmail.com>
2025-05-05 12:17:33 +0200

    Add IItemListingService interface and ItemListingDto class for item listing functionality in Application

---
+++ src/Application/DTOs/ItemListingDto.cs
@@ -0,0 +1,8 @@
+namespace Application.DTOs;
+
+public class ItemListingDto
+{
+    public string Title { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
---
+++ src/Application/Interfaces/IItemListingService.cs
@@ -0,0 +1,10 @@
+using Application.DTOs;
+
+namespace Application.Interfaces;
+
+public interface IItemListingService
+{
+    Task<IEnumerable<ItemListingDto>> GetAllAsync();
+    Task<ItemListingDto> GetByIdAsync(Guid id);
+    Task CreateAsync(ItemListingDto item);
+}
